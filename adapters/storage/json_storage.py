e7e5bbdc70356c37a9803b84ffe8d297a04ce596
vitri <ospan0400@gmail.com>
2026-05-22 15:13:43 +0500

    add json storage logic (parser)

---
+++ adapters/storage/json_storage.py
@@ -0,0 +1,26 @@
+import json
+from typing import Generator
+
+from core.models import GameRecord
+from ports.data_repository import GameDataPort
+
+
+class JSONFileAdapter(GameDataPort):
+
+    def init(self, file_path: str):
+        self._file_path = file_path
+
+    def load_records(self) -> Generator[GameRecord, None, None]:
+
+        with open(self._file_path, "r", encoding="utf-8") as f:
+            raw_data = json.load(f)
+
+        for entry in raw_data:
+            try:
+                yield GameRecord(
+                    player=entry["player"],
+                    score=entry["score"],
+                    date=entry["date"]
+                )
+            except (KeyError, ValueError, TypeError) as e:
+                print(f"[Предупреждение] Запись пропущена: {entry} — Причина: {e}")
