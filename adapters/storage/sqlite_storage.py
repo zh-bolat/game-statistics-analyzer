102707fbe3e10dae52374138fc084b0f6d872364
Ultradodoshka <super.duper.dodo00@gmail.com>
2026-05-25 19:13:16 +0500

    add SQLiteAdapter class

--- adapters/storage/sqlite_storage.py
+++ adapters/storage/sqlite_storage.py
@@ -1,1 +1,7 @@
 from ports.data_repository import GameDataPort
+
+class SQLiteAdapter(GameDataPort):
+    def __init__(self, db_path: str):
+        self.db_path = db_path
+        self.init_db()
+    
