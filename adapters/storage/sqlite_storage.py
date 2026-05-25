32e7fc3191bdf24ae980dc4de8e5200ec924914d
Ultradodoshka <super.duper.dodo00@gmail.com>
2026-05-25 19:21:39 +0500

    add method connect

--- adapters/storage/sqlite_storage.py
+++ adapters/storage/sqlite_storage.py
@@ -1,7 +1,13 @@
+import sqlite3
+
 from ports.data_repository import GameDataPort
 
 class SQLiteAdapter(GameDataPort):
     def __init__(self, db_path: str):
         self.db_path = db_path
         self.init_db()
-    
+
+    def _connect(self) -> sqlite3.Connection:
+        conn = sqlite3.connect(self.db_path)
+        conn.row_factory = sqlite3.Row
+        return conn
